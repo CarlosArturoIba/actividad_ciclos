88217fc8020e9fea4e542465a0e8247bc50c4ed0
CarlosArturoIba <carlos.ibarram@autonoma.edu.co>
2016-11-03 16:53:43 -0500

    Update Ejercicio8.m

--- Ejercicio8.m
+++ Ejercicio8.m
@@ -3,7 +3,7 @@ clc
 %Write a program to compute the sum of the series 12 + 22 + 32 ... such that the sum is
 %as large as possible without exceeding 1000. The program should display how manyterms the sum uses.
 n=0;
-for n=[1:1:100000];
+for n=[0:1:100000];
     y(n)=n^2
     if sum(y)<=1000
         n=(n+1)
